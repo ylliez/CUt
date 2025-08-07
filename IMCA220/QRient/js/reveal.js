a4d5bda0a1474550cd7b34b0b84a7541cd414854
ylliez <illiez@hotmail.com>
2025-08-07 19:54:39 +0200

    scaffold QRient

---
+++ IMCA220/QRient/js/reveal.js
@@ -0,0 +1,12 @@
+const totalSlices = 8;
+
+for (let i = 1; i <= totalSlices; i++) {
+    const unlocked = localStorage.getItem(`slice${i}`);
+    const cell = document.getElementById(`slice${i}`);
+
+    if (unlocked === 'true') {
+        cell.style.backgroundImage = `url('slices/slice_${i}.png')`;
+    } else {
+        cell.style.backgroundColor = '#999';
+    }
+}
